b598b6ee3dd593394a4aaf0bab170b5d4641accb
Vick Javariz <51038821+javavick@users.noreply.github.com>
2019-10-11 21:21:11 -0400

    Fetch list of characters from the Rick and Morty API's 'Characters' endpoint

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,12 +1,27 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
+    function getRequest() {
+      axios
+        .get("https://rickandmortyapi.com/api/character/")
+        .then((response) => {
+          console.log(response);
+          setCharacters(response.data.results);
+        })
+        .catch((error) => {
+          console.log(`Error: ${error}`);
+        });
+    }
+
+    getRequest();
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
   }, []);
+
+  console.log(characters);
 
   return (
     <section className="character-list">
