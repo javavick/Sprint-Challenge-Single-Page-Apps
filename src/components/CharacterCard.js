a49b043d0af38b3bb248192da8aa996e9deddf5b
Vick Javariz <51038821+javavick@users.noreply.github.com>
2019-10-11 21:38:55 -0400

    Create component that renders a card per character

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,14 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard({ character }) {
+  return (
+    <div>
+      <img src={character.image} />
+      <h1>{character.name}</h1>
+      <h2>{character.origin.name}</h2>
+      <p>Species: {character.species}</p>
+      <p>Gender: {character.gender}</p>
+      <p>Status: {character.status}</p>
+    </div>
+  );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import CharacterCard from "./CharacterCard.js";
 
 export default function CharacterList() {
   const [characters, setCharacters] = useState([]);
@@ -25,7 +26,9 @@ export default function CharacterList() {
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      {characters.map((index) => (
+        <CharacterCard character={index} />
+      ))}
     </section>
   );
 }
